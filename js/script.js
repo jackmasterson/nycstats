e4a1816f1ebaa84e548a294225e73e809dfa22c0
JackMasterson <jackmasterson5@gmail.com>
2016-06-20 22:06:32 -0400

    Test to switch to tableau

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-function initializeViz() {
-  var placeholderDiv = document.getElementById("tableauViz");
-  var url = "http://localhost:9090/views/WorldIndicators/GDPpercapita";
-  var options = {
-    width: placeholderDiv.offsetWidth,
-    height: placeholderDiv.offsetHeight,
-    hideTabs: true,
-    hideToolbar: true,
-    onFirstInteractive: function () {
-      workbook = viz.getWorkbook();
-      activeSheet = workbook.getActiveSheet();
-    }
-  };
-  viz = new tableau.Viz(placeholderDiv, url, options);
-}   
-
-
-$(initializeViz);
+function tested(){
+	console.log('tested!');
+};
+tested();
