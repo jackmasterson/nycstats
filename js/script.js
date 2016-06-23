1ca4514a2c7d94c40c78112def0c3e27a36551a7
JackMasterson <jackmasterson5@gmail.com>
2016-06-23 17:56:16 -0400

    Compare between genders and disease, need to sort though

--- js/script.js
+++ js/script.js
@@ -37,168 +37,35 @@ var firstData = {
 	}
 };
 
-
-/*var charted = {
-	// Load the Visualization API and the corechart package.
-      init: function() {
-      	google.load('visualization', '1.0', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-    //  google.setOnLoadCallback(drawComp);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-		//console.log('hey');
-		var info = model.firstDataInfo()[0];
-		var stat = model.firstDataInfo()[0];
-
-		var hispArrM = [];
-		var hispArrF = [];
-
-		var whiteArrM = [];
-		var whiteArrF = [];
-
-		var blackArrM = [];
-		var blackArrF = [];
-
-		var aapiArr = [];
-
-		var resultsMale = [];
-		var resultsFemale = [];
-
-		var heartMale = [];
-		var heartFemale = [];
-
-
-		stat.forEach(function(info){
-				var cause = info.cause_of_death;
-				var sex = info.sex;
-				var ethnicity = info.ethnicity;
-				var HIV = cause === "HUMAN IMMUNODEFICIENCY VIRUS DISEASE";
-				var hep = cause === "VIRAL HEPATITIS";
-				var count = parseInt(info.count);
-				var greater = count>4000;
-		/*		if(greater && male){
-					heartMale.push(info)
-				}
-				if(greater && female){
-					heartFemale.push(info)
-				}*/
-				//console.log(cause);
-	//			if(hep){
-	//				console.log(info);
-	//			}
-		/*		var male = sex === "MALE";
-				var female = sex === "FEMALE";
-				var hispanic = ethnicity === "HISPANIC";
-				var white = ethnicity === "NON-HISPANIC WHITE";
-				var aapi = ethnicity === "ASIAN & PACIFIC ISLANDER";
-				var black = ethnicity === "NON-HISPANIC BLACK";
-
-				if(HIV && male){
-					if(hispanic){
-						hispArrM.push(info)
-					}
-					if(white){
-						whiteArrM.push(info)
-					}
-					if(black){
-						blackArrM.push(info);
-					}
-				}
-				if(HIV && female){
-					if(hispanic){
-						hispArrF.push(info)
-					}
-					if(white){
-						whiteArrF.push(info)
-					}
-					if(black){
-						blackArrF.push(info);
-					}
-				}
-
-			
-		});
-		var hispInfoMale = [hispArrM[0].ethnicity, parseInt(hispArrM[0].count)];
-		var hispInfoFemale = [hispArrF[0].ethnicity, parseInt(hispArrF[0].count)];
-
-		var whiteInfoMale = [whiteArrM[0].ethnicity, parseInt(whiteArrM[0].count)];
-		var whiteInfoFemale = [whiteArrF[0].ethnicity, parseInt(whiteArrF[0].count)];
-
-		var blackInfoMale = [blackArrM[0].ethnicity, parseInt(blackArrM[0].count)];
-		var blackInfoFemale = [blackArrF[0].ethnicity, parseInt(blackArrF[0].count)];
-
-	//	var greaterMale = [heartMale[0].cause_of_death, parseInt(heartMale[0].count)];
-	//	var greaterFemale = [heartFemale[0].cause_of_death, parseInt(heartFemale[0].count)];
-		
-
-		resultsMale.push(hispInfoMale, whiteInfoMale, blackInfoMale);
-		resultsFemale.push(hispInfoFemale, whiteInfoFemale, blackInfoFemale);
-
-			var dataM = new google.visualization.DataTable();
-				dataM.addColumn("string", "Ethnicity", "ethnicity");
-				dataM.addColumn("number", "Number of Deaths", "number");
-				dataM.addRows(resultsMale);
-
-	    	var optionsM = {'title':'HIV Male Deaths, New York City 2007',
-	                       'width':600,
-	                       'height':600};
-
-	        // Instantiate and draw our chart, passing in some options.
-	        var chartM = new google.visualization.PieChart(document.getElementById('chart_div'));
-	        chartM.draw(dataM, optionsM);
-
-			var dataF = new google.visualization.DataTable();
-				dataF.addColumn("string", "Ethnicity", "ethnicity");
-				dataF.addColumn("number", "Number of Deaths", "number");
-				dataF.addRows(resultsFemale);
-
-	    	var optionsF = {'title':'HIV Female Deaths, New York City 2007',
-	                       'width':600,
-	                       'height':600};
-
-	        // Instantiate and draw our chart, passing in some options.
-	        var chartF = new google.visualization.PieChart(document.getElementById('chart_div_two'));
-	        chartF.draw(dataF, optionsF);
-
-      };
-  	}
-};*/
-
 var filter = {
 	
 	init: function() {
 		var that = this;
 		filter.info = model.firstDataInfo()[0];
-
-
-		var ethnicFilter = document.getElementsByClassName('ethnicity-filter')[0];
 		filter.gender = document.getElementsByClassName('gender-filter')[0];
 		filter.causeFilt = document.getElementsByClassName('cause-filter')[0];
-		var genVal = filter.gender.value;
+		filter.genVal = filter.gender.value;
 		filter.causeVal = filter.causeFilt.value;
-		//console.log(filter.causeVal);
 
-		if(genVal === "male"){
+		if(filter.genVal === "male"){
 			filter.male();
 		}
-		if(genVal === "female"){
+		if(filter.genVal === "female"){
 			filter.female();
+		}
+		if(filter.genVal === "compare"){
+			filter.compare();
 		}
 	},
 
 	male: function() {
-		//console.log('male!!');
+
 		filter.genArr = [];
-	//	filter.sex = '';
+
 		this.info.forEach(function(data){
 			filter.sex = data.sex;
 			if(filter.sex === "MALE"){
-				//console.log(data);
+
 				filter.genArr.push(data);
 			}
 		
@@ -207,28 +74,32 @@ var filter = {
 	},
 
 	female: function() {
-	//	console.log('female!!');
+
 		filter.genArr = [];
 		
 		this.info.forEach(function(data){
 			filter.sex = data.sex;
 			if(filter.sex === "FEMALE"){
-				//console.log(data);
+
 				filter.genArr.push(data);
 			}
 		});
-		filter.chartDisease()
+		filter.chartDisease();
+	},
+
+	compare: function() {
+		console.log('shall be compared!');
 	},
 
 	chartDisease: function() {
-	//	console.log(filter.causeVal);
+
 		filter.using = [];
 		filter.genArr.forEach(function(data){
 
 			var cause = data.cause_of_death;
-			//console.log(cause);
+
 			if(cause === filter.causeVal){
-			//	console.log(data);
+	
 				filter.using.push(data);
 			}
 		});
@@ -267,10 +138,8 @@ var filter = {
 	},
 
 	chartIt: function() {
-	//	console.log('time to get to work');
 		google.load('visualization', '1.0', {'packages':['corechart'], 'callback': drawChart});
 
-      // Set a callback to run when the Google Visualization API is loaded.
       	
 		var chartWhite = [filter.chartWhite[0].ethnicity, 
 			parseInt(filter.chartWhite[0].count)];
@@ -278,94 +147,53 @@ var filter = {
 			parseInt(filter.chartBlack[0].count)];
 		var chartHispanic = [filter.chartHispanic[0].ethnicity,
 			parseInt(filter.chartHispanic[0].count)];
-			var pieId = "chart_div_"+filter.causeVal;
-      		var pieEl = "<div class='chart' id='"+pieId+"'></div>";
-      		$('body').append(pieEl);
-      	//	console.log(pieEl);
+			
+	//	var pieId = "chart_div_"+filter.causeVal;
+  	//	var pieEl = "<div class='chart' id='"+pieId+"'></div>";
+  	//	$('body').append(pieEl);
 
-      		var barId = "chart_div_two_"+filter.causeVal;
-      		var barEl = "<div class='chart' id='"+barId+"'></div>";
-      		$('body').append(barEl);
+  	//	var barId = "chart_div_two_"+filter.causeVal;
+  	//	var barEl = "<div class='chart' id='"+barId+"'></div>";
+
+  		var genderId = filter.causeVal+"_chart_"+filter.genVal;
+  		var genderEl = "<div class='chart' id='"+genderId+"'></div>";
+  		//var femaleId = "female_div_"+filter.genVal;
+  	//	$('body').append(barEl);
+
+  		$('body').append(genderEl)
 
 
-
-      	//	$('body').append(barEl);
 		filter.charted = [chartWhite, chartBlack, chartHispanic];
-		console.log(filter.charted);
-      function drawChart() {
-     // 	console.log('drawn!');
 
-		//console.log(document.getElementsByClassName('class'));
+      function drawChart() {
+
       	var data = new google.visualization.DataTable();
       		data.addColumn("string", "Ethnicity", "ethnicity");
       		data.addColumn("number", "Number of Deaths", "number");
       		data.addRows(filter.charted);
 
       		var options = {"title": "2007 "+filter.causeVal+" Deaths in NYC, "+filter.gender.value,
-      					   "width": 400,
-      					   "height": 400};
+      					   "width": 300,
+      					   "height": 300};
 
-      		//var x = 0;
-
-      		var pie = document.getElementById('chart_div_'+filter.causeVal);
-      		//pie.innerHTML = '';
-      		var chartPie = new google.visualization.PieChart(pie);
-      		
-      		var bar = document.getElementById('chart_div_two_'+filter.causeVal);
-
-
-      		//$('body').append(el);
-      		var chartBar = new google.visualization.BarChart(bar);
-      		chartPie.draw(data, options);
-      		chartBar.draw(data, options);
-      //		console.log(pie);
+   //   		var pie = document.getElementById('chart_div_'+filter.causeVal);
+     // 		var bar = document.getElementById('chart_div_two_'+filter.causeVal);
+      		var gen = document.getElementById(filter.causeVal+'_chart_'+filter.genVal);
+      		//var chartPie = new google.visualization.PieChart(pie);
+      		//var chartBar = new google.visualization.BarChart(bar);
+      		var genderBar = new google.visualization.BarChart(gen);
+      		genderBar.draw(data, options);
+      		//chartPie.draw(data, options);
+      		//chartBar.draw(data, options);
       };
-      
+	}
+};
 
-	}/*,
+var clear = {
 
-	white: function() {
-		filter.using = [];
-		this.info.forEach(function(data){
-			var ethnicity = data.ethnicity
-			var white = ethnicity === "NON-HISPANIC WHITE";
-			if(white){
-				filter.using.push(data);
-				console.log(filter.using);
-			}
-		})
-	},
+	init: function() {
 
-	black: function() {
-		filter.using = [];
-		this.info.forEach(function(data){
-			var ethnicity = data.ethnicity
-			var black = ethnicity === "NON-HISPANIC BLACK";
-			if(black){
-
-				filter.using.push(data);
-				console.log(filter.using);
-			}
-		})
-	},
-
-	asian: function() {
-		console.log('asianified');
-	},
-
-	hispanic: function() {
-		filter.using = [];
-		this.info.forEach(function(data){
-			var ethnicity = data.ethnicity
-			var hispanic = ethnicity === "HISPANIC";
-			if(hispanic){
-
-				filter.using.push(data);
-				console.log(filter.using);
-			}
-		})
-		//console.log('his-wahhhht');
-	}*/
+	}
 }
 
 
