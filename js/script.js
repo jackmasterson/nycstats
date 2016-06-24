de28eb726e7c1a969b18975bef068baf38a5e77d
JackMasterson <jackmasterson5@gmail.com>
2016-06-23 21:06:17 -0400

    Can query multiple years

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var model = {
 
 var viewModel = {
 	init: function() {
+	//	ajax.init();
 	//	using.init();
 	//	charted.init();
 	//	filter.init();
@@ -17,35 +18,54 @@ var viewModel = {
 	}
 }
 
-$.ajax({
-	url: "https://data.cityofnewyork.us/resource/uvxr-2jwn.json?year=2007",
-	dataType: "json"
-})
-.done(function(data){
+var ajax = {
 
-	model.firstDataInfo().push(data);
+	init: function(){
 
-	//firstData.init();
-	//filter.init();
+		if(model.firstDataInfo()[0] !== undefined){
+			model.firstDataInfo.removeAll();
+		}
+		ajax.yearSearch = document.getElementsByClassName('year-search')[0];
+		//ajax.yearVal = yearSearch.value;
+		//console.log(ajax.yearVal);
+	//	console.log(yearSearch.value);
+		ajax.yearVal = ajax.yearSearch.value;
+		ajax.url = "https://data.cityofnewyork.us/resource/uvxr-2jwn.json?year="+ajax.yearVal;
+		ajax.render();
+	},
 
-});
+	render: function() {
 
-var firstData = {
+		var that = this;
+		console.log(ajax.url);
+		console.log(ajax.url);
+		$.ajax({
+			url: ajax.url,
+			dataType: "json"
+		})
+		.done(function(data){
 
-	init: function() {
+			model.firstDataInfo().push(data);
+
+			//firstData.init();
+			//filter.init();
+			filter.init();
+		});
 
 	}
-};
+}
 
 var filter = {
 	
 	init: function() {
 		var that = this;
+
+		console.log(model.firstDataInfo()[0]);
 		filter.info = model.firstDataInfo()[0];
 		filter.gender = document.getElementsByClassName('gender-filter')[0];
 		filter.causeFilt = document.getElementsByClassName('cause-filter')[0];
 		filter.chartFilt = document.getElementsByClassName('chart-filter')[0];
-		filter.chartVal = filter.chartFilt.value;
+		//filter.chartVal = filter.chartFilt.value;
 		filter.genVal = filter.gender.value;
 		filter.causeVal = filter.causeFilt.value;
 
@@ -89,10 +109,6 @@ var filter = {
 		filter.chartDisease();
 	},
 
-	compare: function() {
-		console.log('shall be compared!');
-	},
-
 	chartDisease: function() {
 
 		filter.using = [];
@@ -115,6 +131,7 @@ var filter = {
 		filter.chartHispanic = [];
 
 		filter.using.forEach(function(more){
+			//console.log(more);
 			var ethnicity = more.ethnicity;
 			var cause = more.cause_of_death;
 			var white = ethnicity === "NON-HISPANIC WHITE";
@@ -149,20 +166,15 @@ var filter = {
 			parseInt(filter.chartBlack[0].count)];
 		var chartHispanic = [filter.chartHispanic[0].ethnicity,
 			parseInt(filter.chartHispanic[0].count)];
-			
-	//	var pieId = "chart_div_"+filter.causeVal;
-  	//	var pieEl = "<div class='chart' id='"+pieId+"'></div>";
-  	//	$('body').append(pieEl);
 
-  	//	var barId = "chart_div_two_"+filter.causeVal;
-  	//	var barEl = "<div class='chart' id='"+barId+"'></div>";
-
-  		var genderId = filter.causeVal+"_chart_"+filter.genVal;
-  		var genderEl = "<div class='chart' id='"+genderId+"'></div>";
-  		//var femaleId = "female_div_"+filter.genVal;
-  	//	$('body').append(barEl);
-
-  		$('body').append(genderEl)
+  		var barId = filter.causeVal+"_chart_"+filter.genVal+"_bar";
+  		var pieId = filter.causeVal+"_chart_"+filter.genVal+"_pie";
+  		var barEl = "<div class='chart "+filter.genVal+"' id='"+barId+"'></div>";
+		var pieEl = "<div class='chart "+filter.genVal+"' id='"+pieId+"'></div>";
+		console.log(barEl);
+  		$('.first').prepend(pieEl);
+  		console.log(document.getElementsByClassName('first')[0]);
+  		$('.first').prepend(barEl);
 
 
 		filter.charted = [chartWhite, chartBlack, chartHispanic];
@@ -178,20 +190,17 @@ var filter = {
       					   "width": 300,
       					   "height": 300};
 
-   //   		var pie = document.getElementById('chart_div_'+filter.causeVal);
-     // 		var bar = document.getElementById('chart_div_two_'+filter.causeVal);
-      		var gen = document.getElementById(filter.causeVal+'_chart_'+filter.genVal);
-      		//var chartPie = new google.visualization.PieChart(pie);
-      		//var chartBar = new google.visualization.BarChart(bar);
-      		if(filter.chartVal === "PieChart"){
-	      		var genderBar = new google.visualization.PieChart(gen);
-	      	}
-	      	if(filter.chartVal === "BarChart"){
-	      		var genderBar = new google.visualization.BarChart(gen);
-	      	}
-      		genderBar.draw(data, options);
-      		//chartPie.draw(data, options);
-      		//chartBar.draw(data, options);
+      		var pie = document.getElementById(filter.causeVal+'_chart_'+filter.genVal+'_pie');
+      		console.log(pie);
+      		var bar = document.getElementById(filter.causeVal+'_chart_'+filter.genVal+'_bar');
+     	//	if(filter.chartVal === "PieChart"){
+	      		var pieChart = new google.visualization.PieChart(pie);
+	      //	}
+	      	//if(filter.chartVal === "BarChart"){
+	      		var barChart = new google.visualization.BarChart(bar);
+	      	//}
+      		pieChart.draw(data, options);
+      		barChart.draw(data, options);
       };
 	}
 };
